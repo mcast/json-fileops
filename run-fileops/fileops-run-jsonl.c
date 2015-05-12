faf49e8db940ae869d2905075905c54ea37181db
Matthew Astley <mca@sanger.ac.uk>
2015-05-12 23:17:40 +0100

    extract .fileops.jsonl info
    jsmn_typebail 'should' go in jsmn_util.c
      uses new jsmnerr_str, jsmntype_str
    
    need to use new jsmn_nstr for unescaping

--- run-fileops/fileops-run-jsonl.c
+++ run-fileops/fileops-run-jsonl.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <stdbool.h>
 #include "jsmn/jsmn.h"
+#include "jsmn/jsmn_util.h"
 
 /*
  * fileops-run-jsonl --toffset 4564.23 --vfd  < foo.fileops.jsonl
@@ -17,6 +18,15 @@ const char *PROGNAME = "fileops-run-jsonl";
 void bail(char* msg) {
   fprintf(stderr, "%s: %s\n", PROGNAME, msg);
   exit(2);
+}
+
+void jsmn_typebail(int lnum, jsmntype_t want, jsmntype_t got, char *field) {
+  if (want != got) {
+    const char *w = jsmntype_str(want), *g = jsmntype_str(got);
+    fprintf(stderr, "%s: stdin:%d: field %s wanted %s, got %s\n",
+	    PROGNAME, lnum, field, w, g);
+    exit(6);
+  }
 }
 
 double _atod(char *a, char **endptr) {
@@ -112,10 +122,13 @@ int main(int argc, char **argv) {
   while (!feof(stdin)) {
     jsmn_parser p;
     jsmntok_t t[128];
-    int r;
+    int r, k;
 
     /* fileop object props */
+    char *Op = NULL, *fn = NULL, *operr = NULL;
     int vfd = 0; // vfd=0 : strace2jsonl.pl leaves this default when err
+    ssize_t byte_from = -1, byte_to = -1;
+    double elapsed = -1, T = -1;
 
     /* Read a jsonl line */
     lnum ++;
@@ -136,13 +149,74 @@ int main(int argc, char **argv) {
       exit(6);
     }
 
+    /* Deal with a fileops objeect */
     if (debug) printf("stdin:%d: %s", lnum, linebuf);
-    for(i=0; i<r; i++) {
-      char old = linebuf[ t[i].end ];
+    for(i=1, k=t[0].size; i<r && k>0; i += t[i].size + t[i+1].size + 1, k--) {
+      char *keyname = linebuf + t[i].start, *val = linebuf + t[i+1].start;
       linebuf[ t[i].end ] = 0;
-      printf("%d: %d (%s) sz=%d parent=%d\n", i, t[i].type, linebuf + t[i].start, t[i].size, t[i].parent);
-      linebuf[ t[i].end ] = old;
+      linebuf[ t[i+1].end ] = 0;
+
+      if (debug) {
+	printf("i=%d, k=%d: %d (%s) sz=%d parent=%d\n\t%d (%s) sz=%d parent=%d\n",
+	       i, k,
+	       t[i].type,   keyname, t[i].size,   t[i].parent,
+	       t[i+1].type, val,     t[i+1].size, t[i+1].parent);
+      }
+
+      jsmn_typebail(lnum, JSMN_STRING, t[i].type, "(key)");
+      if (!strcmp(keyname, "vfd")) {
+	jsmn_typebail(lnum, JSMN_PRIMITIVE, t[i+1].type, "vfd");
+	vfd = _atoi(val, NULL);
+	if (max_vfd) {
+	  if (vfd > max_vfd || !vfd_filter_or(vfd_filter, vfd, 0)) {
+	    if (debug) printf("skip (vfd=%d)\n", vfd);
+	    vfd = -1;
+	    break;
+	  }
+	}
+      } else if (!strcmp(keyname, "Op")) {
+	jsmn_typebail(lnum, JSMN_STRING, t[i+1].type, "Op");
+	Op = val;
+      } else if (!strcmp(keyname, "fn")) {
+	jsmn_typebail(lnum, JSMN_STRING, t[i+1].type, "fn");
+	fn = val;
+      } else if (!strcmp(keyname, "err")) {
+	jsmn_typebail(lnum, JSMN_STRING, t[i+1].type, "err");
+	operr = val;
+      } else if (!strcmp(keyname, "elapsed")) {
+	jsmn_typebail(lnum, JSMN_PRIMITIVE, t[i+1].type, "elapsed");
+	elapsed = _atod(val, NULL);
+      } else if (!strcmp(keyname, "T")) {
+	jsmn_typebail(lnum, JSMN_PRIMITIVE, t[i+1].type, "T");
+	T = _atod(val, NULL);
+      } else if (!strcmp(keyname, "_op")) {
+	// ignore
+      } else if (!strcmp(keyname, "bytes")) {
+	jsmn_typebail(lnum, JSMN_ARRAY, t[i+1].type, "bytes");
+	if (t[i+1].size != 2) {
+	  fprintf(stderr, "%s: stdin:%d: bytes field array, expected size 2 got size %d\n",
+		  PROGNAME, lnum, t[i+1].size);
+	  exit(6);
+	}
+	jsmn_typebail(lnum, JSMN_PRIMITIVE, t[i+2].type, "bytes[0]");
+	jsmn_typebail(lnum, JSMN_PRIMITIVE, t[i+3].type, "bytes[1]");
+	linebuf[ t[i+2].end ] = 0;
+	linebuf[ t[i+3].end ] = 0;
+	byte_from = _atoi(linebuf + t[i+2].start, NULL);
+	byte_to   = _atoi(linebuf + t[i+3].start, NULL);
+      } else {
+	    fprintf(stderr, "%s: stdin:%d: unexpected key %s\n", PROGNAME, lnum, keyname);
+	exit(6);
+      }
     }
+    if (vfd < 0) continue; // excluded by vfd_filter
+
+    T -= toffset;
+
+    printf("Op = %s\n  T = %f\n  elapsed = %f\n  vfd = %d\n", Op, T, elapsed, vfd);
+    if (fn) printf("  fn=%s\n", fn);
+    if (operr) printf("  err=%s\n", operr);
+    if (byte_from >= 0) printf("  bytes = [%ld .. %ld]\n", byte_from, byte_to);
     putchar(10);
   }
 

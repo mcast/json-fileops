f2893cdcb363fc4a5f2b6ef3f503f23628272d8a
Matthew Astley <mca@sanger.ac.uk>
2015-05-12 22:19:16 +0100

    terminate & dequote for JSMN_STRING type
    also handles blobs

--- run-fileops/jsmn/jsmn_util.h
+++ run-fileops/jsmn/jsmn_util.h
@@ -45,6 +45,28 @@ extern const char *jsmnerr_str(jsmnerr_t r);
 extern jsmnerr_t jsmn_parse_realloc(jsmn_parser *parser, const char *js, size_t len,
 				    jsmntok_t **tokens, size_t *num_tokens);
 
+/**
+ * Turn a parsed string fragment into a nul-terminated de-escaped C
+ * string; in place or copying to a buffer.  Beware, this string could
+ * have internal NUL bytes.
+ *
+ * @param src	The input JSON buffer.
+ * @param t	Parsed token, for which we want text.
+ * @param dst	Destination buffer; NULL to overwrite in-place at (src + t->start).
+ * @param dstn	Size of dst; ignored in destructive mode.
+ * @return Length of resulting string excluding terminating NUL, in bytes.
+ *
+ * If dst[dstn] is not big enough, an incomplete NUL-terminated string
+ * is written.
+ *
+ * If there are somehow character escaping errors in the src[] text
+ * which the parser allowed through, they will be passed on unchanged.
+ *
+ * It should decode wide characters to UTF8, but currently only
+ * decodes single-byte \uNNNN values and passes wide characters
+ * unchanged.
+ */
+extern size_t jsmn_nstr(char *src, jsmntok_t *t, char *dst, size_t dstn);
 
 #ifdef __cplusplus
 }
